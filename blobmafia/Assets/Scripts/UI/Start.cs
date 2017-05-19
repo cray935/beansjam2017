865de130534e09b32eddc3260fdf064a1227f5ed
Fletcher <caschizo4@gmail.com>
2017-05-19 23:38:29 +0200

    init project

---
+++ blobmafia/Assets/Scripts/UI/Start.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour {
+
+    public void GoGame(int LevelIndex)
+    {
+        Application.LoadLevel(LevelIndex);
+    }
+
+
+}
