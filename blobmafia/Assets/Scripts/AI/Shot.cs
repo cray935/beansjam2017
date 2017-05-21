959517f2b62c66625977061e21c6d27dce53d39b
Dustin Heyne <dustin.heyne@gmail.com>
2017-05-21 20:12:16 +0200

    schießerei!

--- blobmafia/Assets/Scripts/AI/Shot.cs
+++ blobmafia/Assets/Scripts/AI/Shot.cs
@@ -5,10 +5,45 @@ using UnityEngine;
 public class Shot : MonoBehaviour
 {
 
+	private bool isPlayerShot;
+
+	void Start ()
+	{
+		isPlayerShot = gameObject.tag == "PlayerShot";
+	}
+
 	void OnCollisionEnter2D (Collision2D coll)
 	{
-		if (coll.collider.tag == "Player" || coll.collider.tag == "Wall") {
+		if (isPlayerShot) {
+			doPlayerShot (coll);
+		} else {
+			doEnemyShot (coll);
+		}
+	}
+
+	private void doPlayerShot (Collision2D coll)
+	{
+		bool hit = coll.collider.tag == "Enemy";
+
+		if (hit || coll.collider.tag == "Wall") {
 			Destroy (gameObject);
+		}
+
+		if (hit) {
+			Destroy (coll.gameObject);
+		}
+	}
+
+	private void doEnemyShot (Collision2D coll)
+	{
+		bool hit = coll.collider.tag == "Player";
+
+		if (hit || coll.collider.tag == "Wall") {
+			Destroy (gameObject);
+		}
+
+		if (hit) {
+			// TODO Spieler muss Leben verlieren oder sterben
 		}
 	}
 }
---
+++ blobmafia/Assets/Scripts/Player/PlayerAttackController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackController : MonoBehaviour
+{
+	public Rigidbody2D shotPrefab;
+
+	public float shootingRate = 1f;
+	public float shootSpeed = 0.5f;
+
+	private Transform shootPos;
+	private float shootCooldown = 0f;
+	// Kann man ja später mal richtig implementieren..
+	private bool looksLeft = false;
+
+	void Start ()
+	{
+		shootPos = GetComponent<Transform> ();
+	}
+
+	void Update ()
+	{
+		if (shootCooldown > 0) {
+			shootCooldown -= Time.deltaTime;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Attack ();
+		}
+	}
+
+	public void Attack ()
+	{
+		if (CanAttack) {
+
+			shootCooldown = shootingRate;
+
+			if (looksLeft) {
+				Rigidbody2D clone = (Instantiate (shotPrefab, shootPos.position, shotPrefab.transform.rotation) as Rigidbody2D);
+				clone.AddForce (new Vector2 (-shootSpeed, 0), ForceMode2D.Force);
+			} else {
+				Rigidbody2D clone = (Instantiate (shotPrefab, transform.position + 1.0f * transform.forward, transform.rotation) as Rigidbody2D);
+				clone.AddRelativeForce (new Vector2 (shootSpeed, 0), ForceMode2D.Force);
+			}
+
+		}
+	}
+
+	public bool CanAttack {
+		get {
+			return shootCooldown <= 0f;
+		}
+	}
+}
