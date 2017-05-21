86f2cfe9789d704b23875f4753e7b68d120bff80
Dustin Heyne <dustin.heyne@gmail.com>
2017-05-21 16:01:41 +0200

    dialog kram für scene 1

---
+++ blobmafia/Assets/Scripts/Controller/DialogStarter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogStarter : MonoBehaviour
+{
+	public DialogBoxController dialogController;
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if (collider.tag == "GohstWall")
+			dialogController.enableDialogBox ();
+	}
+}
