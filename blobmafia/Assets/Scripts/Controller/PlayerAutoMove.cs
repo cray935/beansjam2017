de031436fa0bad83b1ae775bb7071f8ff2eec6bb
Fletcher <caschizo4@gmail.com>
2017-05-21 13:13:33 +0200

    FletcherArbeit
    Fletcher

---
+++ blobmafia/Assets/Scripts/Controller/PlayerAutoMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAutoMove : MonoBehaviour {
+
+    public PlayerMove moveScript;
+
+    public int movesMax = 10;
+
+    int moved = 0;
+
+    float timeleft = 5.0f;
+
+    bool stopMove = false;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (stopMove)
+        {
+            return;
+        }
+        
+        if (timeleft > 0)
+        {
+            timeleft -= Time.deltaTime;
+            return;
+        }
+
+        if (moved < movesMax)
+        {
+            moveScript.moveRight();
+            moved++;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "GohstWall")
+        {
+            stopMove = true;
+        }
+    }
+
+}
